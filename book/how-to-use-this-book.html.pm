a68cbcd2c6e3265619e43fbad9bc9821b176f44d
Antti Halla <antti.halla@gmail.com>
2015-10-18 18:48:26 +0300

    Add how to use, remove additional resources, tune chapter heaadings

---
+++ book/how-to-use-this-book.html.pm
@@ -0,0 +1,12 @@
+#lang pollen
+
+◊h1{How to use this book}
+
+
+Keyboard:
+* Left arrow --- Previous page
+* Right arrow --- Next page
+* Esc --- Home page
+* Alt + Arrow up --- Chapter index (or home page if none exists)
+
+
