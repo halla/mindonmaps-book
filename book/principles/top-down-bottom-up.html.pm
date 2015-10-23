520255d95897f272fcdae225acbc24de0bc71d2a
Antti Halla <antti.halla@gmail.com>
2015-10-23 09:57:07 +0300

    Top-down-bottom-up, editing

--- book/principles/top-down-bottom-up.html.pm
+++ book/principles/top-down-bottom-up.html.pm
@@ -3,8 +3,14 @@
 
 ◊h1{Top-down, bottom-up}
 
-Sometimes you start with generating a lot of ideas and only later organize them to categories, when you start noticing patterns. On the other hand, sometimes you already have the categories, a clearly defined set of questions to answer or boxes to be fill. 
+Sometimes you start with generating a lot of ideas and only later organize them to categories, when you start noticing patterns. On the other hand, sometimes you already have the categories, a clearly defined set of questions to answer or boxes to be fill. These can be called ◊em{bottom-up} and ◊em{top-down} approaches respectively.
 
-To keep an open mind, let the content drive the structure. First create a collection of ideas, then organize it. If you want to do any out-of-the-box thinking the easiest way to do it is to not put yourself in a box in the first place. By creating a table of contents, an outline or a categorization, you are in effect defining the box. Predefining the structure will get in your way by directing and limiting your thinking.
+◊blockquote{ If you want to think out-of-the-box, don't put yourself in a box in the first place.}
 
-Directing and limiting can also be a good thing, especially when you are repeating something. You don't need creativity to do routine tasks. The more you repeat something, the less you'll probably want to think about it! Nevertheless, you'll want to make sure that every aspect gets covered. Checklists and standard forms are useful for that. 
+To keep an open mind, start from the bottom and let the content drive the structure. First create a collection of ideas, then organize it.  If you want to think out-of-the-box, don't put yourself in a box in the first place. By creating a table of contents, an outline or a categorization, you are in effect defining the box. Predefining the structure will get in your way by directing and limiting your thinking.
+
+Notice that the bottom-up approach is basically impossible with pen and paper mind maps, because editing is so hard. You move towards the top by grouping items together into categories. You do this ◊em{after} you've already written things down, which means heavy editing. If you need to do this on paper, a better way would be to use sticky notes which you can move around easily.
+
+Directing and limiting --- moving top-down --- can also be a good thing, especially when you are repeating something. Effeciency becomes more valuable than creativity. The more you repeat something, the less you'll probably want to think about it! You can define the categories first, and then fill them out like a checklist. Nevertheless, you'll want to make sure that every aspect gets covered. 
+
+
