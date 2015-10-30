c413fcb3b5908a10c43bce9c4de9d005fad724a5
Antti Halla <antti.halla@gmail.com>
2015-10-30 09:48:45 +0200

    Ninja skills section edit and additions

--- book/mind-map-ninja-skills/physical-skills/touch-typing.html.pm
+++ book/mind-map-ninja-skills/physical-skills/touch-typing.html.pm
@@ -2,5 +2,11 @@
 
 ◊h1{Touch typing}
 
-Even if you are not the sharpest pencil in the box, the speed of your thinking is much faster than the speed of your typing. The faster you write, the less likely you are to get bored waiting for your fingers to write all those things you want to appear on the screen. You don't want to look at your fingers all the time either. You want to keep your eyes on the mind map, looking for connections between ideas, staying focused on the thing you are thinking about. Even if you already know the basics of touch typing, when was the last time you took a typing test? If you score less than 400 characters per minute (cpm) there's still room for improvement. The fastest typists can achieve speeds of over 1000 cpm. 
+Do yourself a favor and learn touch typing now, if you haven't already done so. The keyboard is in many cases still the main interface for interacting with computers. Whether it is mind mapping, writing, programming or chatting online, the ability to to type fast without thinking about it is a key to actually ◊em{enjoy} working with a computer.  
+
+To think fast you need to type fast. Even if you are not the sharpest pencil in the box, the speed of your thinking is much faster than the speed of your typing. The faster you write, the faster you can move on to the next thought. You're also less likely you are to get bored waiting for your fingers to write all those things you want to appear on the screen. You don't want to look at your fingers all the time either. You want to keep your eyes on the mind map, looking for connections between ideas, staying focused on the thing you are thinking about. 
+
+If you already know the basics of touch typing, when was the last time you took a typing test? If you score less than 400 characters per minute (cpm) there's still room for improvement. Did you know the fastest typists can achieve speeds of over 1000 cpm?
+
+
 
