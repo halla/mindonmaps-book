27ea45575dfe874be09ecf2f75cae048942cc2f6
Antti Halla <antti.halla@gmail.com>
2015-11-23 16:45:07 +0200

    Add tips on using a distraction free mode

--- book/introduction/how-to-use-this-book.html.pm
+++ book/introduction/how-to-use-this-book.html.pm
@@ -2,13 +2,24 @@
 
 ◊h1{How to use this book}
 
-You can read the book in order, or you can pick just those topics that draw your interests. Whichever works for you. 
+You can read the book in order, or you can pick just those topics that draw your interests. Whichever works for you. However, I'd suggest that you read the introduction chapter first to get a better idea of what the book is about. 
 
-There are some keyboard shotcuts to help navigating the book:
+
+◊h2{Go full-screen}
+
+To get a distraction free reading-experience, make sure the book page fills the ◊em{whole} screen, leaving nothing else, even on the edges. Getting easily distracted is one of the challenges of reading anything online.
+
+Different devices, operating systems and the browsers have their own ways of achieving distraction free mode. Look through the menus for options like ◊em{"full screen", "hide toolbar", "distraction free"}. This might feel like an insignificant detail, but it can have a massive impact on your reading experience. 
+
+
+◊h2{Use keyboard shortcuts}
+
+There are some keyboard shotcuts to help navigating the book. Flipping through the pages is more fun using the arrow buttons than trying to aim at the link texts with a mouse. If you have a touch screen device, try swiping right and left.
 
 * Left arrow --- Previous page
 * Right arrow --- Next page
 * Esc --- Home page
 * Alt + Arrow up --- Chapter index (or home page if none exists)
+
 
 
