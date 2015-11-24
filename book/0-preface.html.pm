31491ec6364f16fd5d9cf916423c44a3cbd84f14
Antti Halla <antti.halla@gmail.com>
2015-11-24 09:59:10 +0200

    Leading words for preface, play with subtitle

--- book/0-preface.html.pm
+++ book/0-preface.html.pm
@@ -1,14 +1,14 @@
 #lang pollen
 ◊(define-meta page-title "Preface")
 ◊(define-meta page-subtitle "How I got into mind maps")
- 
+
+◊page-subtitle{◊(hash-ref metas 'page-subtitle)} 
 ◊h1['class:"chapter-title"]{◊(hash-ref metas 'page-title)}
-◊page-subtitle{◊(hash-ref metas 'page-subtitle)}
 
 
 	
 
-It’s a Saturday morning back in 2003. Nothing special to do, a day just for myself. I make a big cup of coffee for myself, and sit down and open my laptop computer.
+◊leading-words{It’s a Saturday} morning back in 2003. Nothing special to do, a day just for myself. I make a big cup of coffee for myself, and sit down and open my laptop computer.
 
 Some months earlier I had found a free computer program for mind mapping. It was more like a curiosity for me than a serious tool. It was just fun to make all kinds of lists and organize them. A list I was about to do this time, too. I started to write down everything I remembered that had ever happened to me in my whole life. It turned out to be quite a lot.
 
@@ -16,5 +16,5 @@ It was an amazing experience. I started by listing places I’d been to, schools
 
 Day by day I started to find new ways to use the mind maps. They were an immensely practical tool, and I started to use them every day and for everything. Now, after 10 years I am still using them all the time. I don’t even think about it anymore, just as I don’t think about driving a car. It just happens.
 
-Mind mapping transformed my life, and I would love to share a part of that experience and excitement with you. As much as this blog is about mind maps, this is also about thinking about things in general. Mind maps are for me, in the end, just a means to extend my ability to think.
+Mind mapping transformed my life, and I would love to share a part of that experience and excitement with you. As much as this book is about mind maps, this is also about thinking about things in general. Mind maps are for me, in the end, just a means to extend my ability to think.
 
